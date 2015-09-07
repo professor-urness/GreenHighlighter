ff39eadc926d523c4ff9a5c1ea97afa0d8935621
Professor Urness <professor.urness@gmail.com>
2015-09-07 14:08:10 -0500

    possible conflict

--- app/src/main/java/com/urness/greenhighlighter/MainActivity.java
+++ app/src/main/java/com/urness/greenhighlighter/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //test test
         //How about this for a test!!!?
+
+        /*this might create a conflict!!!*/
     }
 
     @Override
