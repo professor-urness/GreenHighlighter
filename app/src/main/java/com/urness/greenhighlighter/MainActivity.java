4588920aec31dba1b1dc624504022b32ab0c9378
Timothy Urness <timothy.urness@drake.edu>
2015-09-09 09:50:47 -0500

    added blah

--- app/src/main/java/com/urness/greenhighlighter/MainActivity.java
+++ app/src/main/java/com/urness/greenhighlighter/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //test test
         //more testing
+
+        //blah blah blah
     }
     
     @Override
